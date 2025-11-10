db74c74feb8352516ff534e6e6cdd97380c5e559
Petar Radojicic <steroporstop@gmail.com>
2025-11-10 15:31:29 +0100

    main: R8/proguard fix

--- plugins/withProguardRules.js
+++ plugins/withProguardRules.js
@@ -1,5 +1,5 @@
 // plugins/withProguardRules.js
-const { withDangerousMod } = require('@expo/config-plugins');
+const { withDangerousMod } = require('expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
